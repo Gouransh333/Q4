3680a18a4a0f62a955ae5044ed9afcd912739194
Gouransh333 <gouransh333@gmail.com>
2025-08-25 18:14:16 +0000

    Add files via upload

---
+++ Q4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int r;
+float a,c;
+printf("Enter the radius \n");
+scanf("%d",&r);
+a=3.14*r*r;
+c=2.0*3.14*r;
+printf("Area= %.2f, Circumference= %.2f\n",a,c);
+return 0;
+}
+
